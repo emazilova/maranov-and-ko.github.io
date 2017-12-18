d92ab31f2fdd1584b07c09a17efb5d1b5872cf6f
maranov-and-ko <katbka07@mail.ru>
2017-12-18 15:23:34 +0300

    refactoring

--- speech-recognition/js/speech.js
+++ speech-recognition/js/speech.js
@@ -17,7 +17,30 @@ const CURRENCY_INFO = {
     },
 };
 
+function mobileAndTabletcheck() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+}
+
 window.onload = function () {
+    const listFrom = document.querySelector('ul[for="currency-from"]');
+    const listTo = document.querySelector('ul[for="currency-to"]');
+
+    for(let currency in CURRENCY_INFO) {
+        const liFrom = document.createElement('li');
+        liFrom.setAttribute('class', 'mdl-menu__item');
+        liFrom.setAttribute('data-val', currency);
+        liFrom.innerText = CURRENCY_INFO[currency].symbol;
+        listFrom.appendChild(liFrom);
+
+        const liTo = document.createElement('li');
+        liTo.setAttribute('class', 'mdl-menu__item');
+        liTo.setAttribute('data-val', currency);
+        liTo.innerText = CURRENCY_INFO[currency].symbol;
+        listTo.appendChild(liTo);
+    }
+
     const micButton = document.getElementById('start-recording');
     const Recognition = window.SpeechRecognition ||
         window.webkitSpeechRecognition ||
@@ -60,23 +83,6 @@ window.onload = function () {
                 recognition.stop();
             }
         };
-    }
-
-    const listFrom = document.querySelector('ul[for="currency-from"]');
-    const listTo = document.querySelector('ul[for="currency-to"]');
-
-    for(let currency in CURRENCY_INFO) {
-        const liFrom = document.createElement('li');
-        liFrom.setAttribute('class', 'mdl-menu__item');
-        liFrom.setAttribute('data-val', currency);
-        liFrom.innerText = CURRENCY_INFO[currency].symbol;
-        listFrom.appendChild(liFrom);
-
-        const liTo = document.createElement('li');
-        liTo.setAttribute('class', 'mdl-menu__item');
-        liTo.setAttribute('data-val', currency);
-        liTo.innerText = CURRENCY_INFO[currency].symbol;
-        listTo.appendChild(liTo);
     }
 
     const buttonConvertDirection = document.getElementById('convert-direction');
@@ -129,7 +135,7 @@ function getCurrenciesFromInputs() {
         resultInput = currencyFromValue;
     }
 
-    if(!sum || !currencyFrom || !currencyTo) {
+    if(!sum || isNaN(sum) || !currencyFrom || !currencyTo) {
         return;
     }
 
@@ -140,7 +146,8 @@ function getCurrenciesFromInputs() {
 function getCurrenciesFromVoice(text) {
     const result = document.getElementById('result');
 
-    const foundWords = text.match(/(переведи|сколько\s+будет)\s+([\d]+)\s+([а-я.]+)\s+в\s+([а-я]+)/i);
+    const foundWords = text.match(/(переведи|сколько\s+будет|)\s+([\d]+)\s+([а-я.]+)\s+в\s+([а-я]+)/i);
+
     if (foundWords === null || foundWords[2] === undefined || foundWords[3] === undefined || foundWords[4] === undefined) {
         result.innerHTML += `Не удалось распознать команду: ${text}.<br><br>`;
     } else {
@@ -148,8 +155,12 @@ function getCurrenciesFromVoice(text) {
         const currencyFrom = Object.keys(CURRENCY_INFO).find(key => CURRENCY_INFO[key].names.indexOf(foundWords[3]) >= 0);
         const currencyTo = Object.keys(CURRENCY_INFO).find(key => CURRENCY_INFO[key].names.indexOf(foundWords[4]) >= 0);
 
-        if (!currencyFrom || !currencyTo || isNaN(sum)) {
-            result.innerHTML += 'Невозможно сконвертировать данную валюту.<br><br>';
+        result.innerHTML += `${text}<br>`;
+
+        if (!currencyFrom) {
+            result.innerHTML += `Невозможно сконвертировать данную валюту: ${foundWords[3]}.<br><br>`;
+        } else if(!currencyTo) {
+            result.innerHTML += `Невозможно сконвертировать данную валюту: ${foundWords[4]}.<br><br>`;
         } else {
             const convertedValue = convertCurrency(sum, currencyFrom, currencyTo);
 
@@ -165,7 +176,7 @@ function getCurrenciesFromVoice(text) {
             document.getElementById('currencyToValue').value = convertedValue;
             document.getElementById('currencyToValue').parentNode.classList.add('is-dirty');
 
-            result.innerHTML += `${text}<br>Результат: ${convertedValue + CURRENCY_INFO[currencyTo].symbol}<br><br>`;
+            result.innerHTML += `Результат: ${convertedValue + CURRENCY_INFO[currencyTo].symbol}<br><br>`;
         }
     }
 }
@@ -199,10 +210,4 @@ function convertToRub(currencyFrom, sum) {
     const nominalFrom = currencyFromInfo.Nominal;
     const valueFrom = currencyFromInfo.Value;
     return sum * (valueFrom / nominalFrom);
-}
-
-function mobileAndTabletcheck() {
-    let check = false;
-    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-    return check;
 }
